d6ac445c697f20e8c0692d82c58510d13082f0bc
Atharv <atharvbhandare2001@gmail.com>
2022-01-27 16:54:31 +0530

    conditionally rendering statements in App.js and Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 // hooks
 import { useLogout } from "../hooks/useLogout";
-
+import { useAuthContext } from "../hooks/useAuthContext"
 // Styles & Images
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 
 export default function Navbar() {
   const { logout, isPending } = useLogout();
-
+  const { user } = useAuthContext();
   return (
     <div className="navbar">
       <ul>
@@ -18,25 +18,30 @@ export default function Navbar() {
           <img src={Temple} alt="project logo" />
           <span>TheProjectHandler</span>
         </li>
-
-        <li>
-          <Link to="login">Login</Link>
-        </li>
-        <li>
-          <Link to="signup">signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Logging out...
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="login">Login</Link>
+            </li>
+            <li>
+              <Link to="signup">signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging out...
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   );
