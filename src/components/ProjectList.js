0a8cb4b115395c1dbacb4b4176d3fe221bd0048d
Atharv <atharvbhandare2001@gmail.com>
2022-01-31 16:18:41 +0530

    fetching projects

---
+++ src/components/ProjectList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Styles
+import './ProjectList.css'
+export default function ProjectList({projects}) {
+  return(
+    <div>
+        {projects.length === 0 && <p>No projects yet!</p>}
+        {projects.map(project => (
+            <div key={project.id}>{project.name}</div>
+        ))}
+    </div>
+  );
+}
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,17 @@
+// hooks
+import { useCollection } from "../../hooks/useCollection"
 // styles
 import './Dashboard.css'
+// components
+import ProjectList from "../../components/ProjectList"
 
 export default function Dashboard() {
+  const {document, error} = useCollection('projects')
   return (
     <div>
-      Dashboard
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {document && <ProjectList projects={document} />}
     </div>
   )
 }
