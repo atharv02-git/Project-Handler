5b98a376c7d74d1b8e32cfe091ec7343c2e201b8
Atharv <atharvbhandare2001@gmail.com>
2022-01-31 23:44:50 +0530

    Making a Project List template in /ProjectList.js

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "./Avatar";
 //Styles
-import './ProjectList.css'
-export default function ProjectList({projects}) {
-  return(
-    <div>
-        {projects.length === 0 && <p>No projects yet!</p>}
-        {projects.map(project => (
-            <div key={project.id}>{project.name}</div>
-        ))}
+import "./ProjectList.css";
+export default function ProjectList({ projects }) {
+  return (
+    <div className="project-list">
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map((project) => (
+        <Link to={`projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          {/** as date is in timeStamp form we need to convert it into js string */}
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                  <p>{user.displayName}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
