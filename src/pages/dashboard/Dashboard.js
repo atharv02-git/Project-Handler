45996b170136484158149cf76b50991d5d9bcf2f
Atharv <atharvbhandare2001@gmail.com>
2022-02-03 15:49:25 +0530

    Making filter

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { useCollection } from "../../hooks/useCollection"
 import './Dashboard.css'
 // components
 import ProjectList from "../../components/ProjectList"
+import ProjectFilter from "./ProjectFilter"
 
 export default function Dashboard() {
   const {document, error} = useCollection('projects')
@@ -11,6 +12,7 @@ export default function Dashboard() {
     <div>
       <h2 className='page-title'>Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {document && <ProjectFilter /> }
       {document && <ProjectList projects={document} />}
     </div>
   )
