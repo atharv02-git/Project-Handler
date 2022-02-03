b94ad55c915e2b940916d6bfa8000fdffe1d266f
Atharv <atharvbhandare2001@gmail.com>
2022-02-03 14:23:17 +0530

    deleting project only by the user created the same

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Avatar from "../../components/Avatar";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+import { useNavigate } from 'react-router-dom';
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects');
+  const { user } = useAuthContext();
+  const navigate = useNavigate()
+
+  const clickHandler = () => {
+    deleteDocument(project.id);
+    navigate('/')
+  };
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">{project.dueDate.toDate().toDateString()}</p>
         <p className="details">{project.details}</p>
         <h4 className="assigned-to">Project is assigned to:</h4>
@@ -18,6 +31,12 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {/* we need to show the mark as complete button only if the logged in user === the user created the project*/}
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={clickHandler}>
+          Mark as complete
+        </button>
+      )}
     </div>
   );
 }
